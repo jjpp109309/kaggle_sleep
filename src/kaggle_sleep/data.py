f532b3710f06045ac630bb4730e7ad48c7971907
J. Jaime Paredes <jjpp109309@gmail.com>
2023-10-05 22:54:33 -0600

    load data using polars

--- src/kaggle_sleep/data.py
+++ src/kaggle_sleep/data.py
@@ -1,3 +1,5 @@
+import polars as pl
+
 from pyarrow import Table
 from pyarrow.parquet import read_table
 
@@ -5,5 +7,11 @@ from pyarrow.parquet import read_table
 def load_series(path: str) -> Table:
 
     data = read_table(path)
+
+    return data
+
+
+def load_data(path: str) -> pl.DataFrame:
+    data = pl.read_parquet(path, use_pyarrow=True)
 
     return data
